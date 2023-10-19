478c82da97003bb769f27223eca93d7ac849dfc5
Sanaz Kazeminia <sanazkazeminia97@gmail.com>
2023-10-19 22:44:00 +0100

    class created in protocol.py for dosing regimes

--- pkmodel/protocol.py
+++ pkmodel/protocol.py
@@ -1,17 +1,48 @@
-#
-# Protocol class
-#
+# in protocol.py
 
 class Protocol:
-    """A Pharmokinetic (PK) protocol
+    def __init__(self, dose_times=[], dose_amounts=[]):
+        """
+        Initialize the dosing protocol with times and amounts.
+        """
+        self.dose_times = dose_times
+        self.dose_amounts = dose_amounts
 
-    Parameters
-    ----------
+    def dose(self, t):
+        """
+        Default dose function. Returns the dose amount if t matches a dose time, 0 otherwise.
+        """
+        for i, dose_time in enumerate(self.dose_times):
+            if t == dose_time:
+                return self.dose_amounts[i]
+        return 0
 
-    value: numeric, optional
-        an example paramter
-
+class Subcutaneous(Protocol):
     """
-    def __init__(self, value=43):
-        self.value = value
+    Subcutaneous injection protocol.
+    """
+    def __init__(self, X=0.0, dose_times=[], dose_amounts=[]):
+        super().__init__(dose_times, dose_amounts)
+        self.X = X
 
+    def dose(self, t):
+        """
+        Overrides the default dose function. 
+        Adds a constant X to the dose value (if any) at time t.
+        """
+        return super().dose(t) + self.X
+
+class IVBolus(Protocol):
+    """
+    IV bolus injection protocol.
+    """
+    def __init__(self, X=0.0, dose_times=[], dose_amounts=[]):
+        super().__init__(dose_times, dose_amounts)
+        self.X = X
+
+    def dose(self, t):
+        """
+        Overrides the default dose function. 
+        Adds a constant X to the dose value (if any) at time t.
+        """
+        return super().dose(t) + self.X
